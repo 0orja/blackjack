0eb821c2e71c08f0647a6af6e79a9a62daae06a9
oorjamajgaonkar <ojm238@nyu.edu>
2019-11-13 15:33:12 -0500

    hide start page

--- src/app.js
+++ src/app.js
@@ -4,3 +4,4 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
+console.log('server started');
--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,1 +1,13 @@
 // main.js
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+	console.log('main');
+	const playBtn = document.querySelector('.playBtn');
+	console.log(playBtn);
+	playBtn.addEventListener('click', function(evt) {
+		const startPage = document.querySelector('.start');
+		startPage.classList.toggle('hide');
+		evt.preventDefault();
+	})
+}
